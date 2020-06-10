3d608d25188645819638d21b6a3de537b5229590
Abhishek Rawat <36125867+abhishekrwt@users.noreply.github.com>
2020-06-10 15:57:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -28,26 +28,6 @@ captions_train_flat = open('captions_train_flat.pkl','rb')
 captions_train_flat = pickle.load(captions_train_flat)
 num_words = 10000
 
-class TokenizerWrap(Tokenizer):
-    def __init__(self, texts, num_words=None):
-        Tokenizer.__init__(self, num_words=num_words)
-        self.fit_on_texts(texts)
-        self.index_to_word = dict(zip(self.word_index.values(), self.word_index.keys()))
-
-    def token_to_word(self, token):
-        word = " " if token == 0 else self.index_to_word[token]
-        return word 
-
-    def tokens_to_string(self, tokens):
-        words = [self.index_to_word[token] for token in tokens if token != 0]
-        text = " ".join(words)
-        return text
-    
-    def captions_to_tokens(self, captions_listlist):
-        tokens = [self.texts_to_sequences(captions_list) for captions_list in captions_listlist]
-        return tokens
-
-
 tokenizer = TokenizerWrap(texts=captions_train_flat, num_words=num_words)
 
 image_model = VGG16(include_top=True, weights='imagenet')
@@ -130,7 +110,6 @@ def predict():
 		print(description)
 		return jsonify(description)
 		# return render_template('index.html', prediction = description)
-	print("nahi")
 
 if __name__ == "__main__":
 	print("when")
